628ede5957b8f3e767c63e8c79e207cc8e79bc55
haixiangyan <haixiang6123@gmail.com>
2022-12-17 14:36:31 +0800

    feat: 完成数字大屏展示

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
+import NumberDisplay from "./components/NumberDisplay";
+import styles from './styles.module.scss';
+import dayjs from "dayjs";
 
+const getRandomNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+// 1. 做一个垂直的 0-9 div
+// 2. 滚动到 7 时，div 就要垂直移动到 7 * height 个单位
 function App() {
+  const [time, setTime] = useState<number>(Date.now().valueOf());
+  const [numberString, setNumberString] = useState<string>('0.123');
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(Date.now().valueOf());
+    }, 1000);
+  }, []);
+
+  const timeStr = useMemo(() => {
+    return dayjs(time).format('HH:mm:ss');
+  }, [time]);
+
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     setNumberString(getRandomNumber(100, 999).toString());
+  //   }, 1000);
+  // }, []);
+
   return (
-    <div className="App">
-      Hello
+    <div className={styles.app}>
+      <NumberDisplay numberString={timeStr} />
     </div>
   );
 }
---
+++ src/components/NumberDisplay/index.tsx
@@ -0,0 +1,38 @@
+import {FC, useState} from "react";
+import styles from './styles.module.scss';
+
+const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const digitRegexp = /\d/;
+
+interface Props {
+  numberString: string; // '123'
+}
+
+const NumberDisplay: FC<Props> = (props) => {
+  const { numberString } = props;
+
+  return (
+    <div className={styles.numberDisplay}>
+      {/* ['1', '2', '3']*/}
+      {numberString.split('').map(digitStr => (
+        <div className={styles.digitWrapper}>
+          {
+            digitRegexp.test(digitStr) ? (
+              // 展示数字
+              <span className={styles.digitList} style={{ transform: `translate(-50%, ${-Number(digitStr) * 32}px)` }}>
+                {digits.map(digit => (
+                  <span className={styles.digit} key={digit}>{digit}</span>
+                ))}
+              </span>
+            ) : (
+              // 展示普通字符
+              <span>{digitStr}</span>
+            )
+          }
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default NumberDisplay;
